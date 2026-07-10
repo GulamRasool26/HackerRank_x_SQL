31d19fe846875876147c7f8390287c21cc862ff6
GulamRasool26 <gulamrasool2026@gmail.com>
2026-07-10 16:12:33 +0500

    10 July 7th

---
+++ drawing_triangle_recursive.sql
@@ -0,0 +1,22 @@
+-- P(R) represents a pattern drawn by Julia in R rows. 
+-- The following pattern represents P(5):
+-- * 
+-- * * 
+-- * * * 
+-- * * * * 
+-- * * * * *
+-- Write a query to print the pattern P(20).
+
+WITH RECURSIVE NUM AS 
+(
+    SELECT 1 AS n
+    
+    UNION ALL
+    
+    SELECT n +1 
+    FROM NUM
+    WHERE n < 20
+    
+)
+SELECT REPEAT(' *',n)
+FROM NUM
